4ad09d43a1b0126d6d326fcd990d94b555c0ea18
kohei-san <kohei.miyatake@gmail.com>
2022-05-31 09:38:20 +0900

    Create equalityOperator.php

---
+++ equalityOperator.php
@@ -0,0 +1,12 @@
+<?php
+// trueの場合は1と出力される
+print "a" == "a";
+print "\n";
+print "a" != "b";
+print "\n";
+
+// falseの場合は出力はなかった。
+print "a" == "b";
+$result = "a" == "b";
+print $result . "\n";
+print false;
