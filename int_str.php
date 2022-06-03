eb0a8783515a450bcf6b8049a2faa8d166add4fd
kohei-san <kohei.miyatake@gmail.com>
2022-06-03 22:21:11 +0900

    Create int_str.php

---
+++ int_str.php
@@ -0,0 +1,14 @@
+<?php
+if ('5member' < 44) {
+    print "44";
+} else {
+    print "5member";
+}
+
+if ('3member' < 44) {
+    print "44";
+} else {
+    print "3member";
+}
+
+// 文字列の1文字目のみ比較されているようだ
